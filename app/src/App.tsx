dd748331386bd81ec974d077e5dc7ee0a974a521
latrasis <>
2024-03-04 15:46:30 +0300

    basic withdrawal

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,6 +7,8 @@ import { ComboBox, ComboBoxProps } from '@/components/ui/combobox';
 import { Button } from '@/components/ui/button';
 import { Input } from './components/ui/input';
 import { useEscrow } from './lib/useEscrow';
+import { Card, CardContent, CardHeader } from './components/ui/card';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table';
 
 const APP_OPTION: ComboBoxProps["values"] = [
   { value: "0x1", label: "mainnet" },
@@ -17,10 +19,11 @@ const APP_OPTION: ComboBoxProps["values"] = [
 export default function App() {
   const { sdk, connected, balance, balanceProcessing, provider, account } = useSDK();
   const { chainId, connect } = useChainId()
-  const { escrow, deployEscrow, deployNft, nfts, mintToken, sendToEscrow } = useEscrow()
+  const { escrow, deployEscrow, deployNft, nfts, mintToken, sendToEscrow, withdrawEscrow } = useEscrow()
 
   const [newNft, setnewNft] = useState("")
   const [newNftId, setnewNftId] = useState(1)
+
 
   const reqTopup = async () => {
     try {
@@ -39,38 +42,112 @@ export default function App() {
     }
   }
 
-  let [token, setToken] = useState<string|undefined>()
-  let [nft, setNft] = useState<string|undefined>()
-  let nftTokensIndex = nfts.findIndex(r => r.addr.toLowerCase() == nft)
-  let targetNft = nfts[nftTokensIndex]
+  let [token, setToken] = useState<string | undefined>()
+  let [nft, setNft] = useState<string | undefined>()
 
-  useEffect(() => {
-    reqTopup()
-  }, [])
+  useEffect(() => { reqTopup() }, [])
 
   return (
-    <div className="App">
-      <ComboBox value={chainId ?? ""} values={APP_OPTION} setValue={id => connect(id)}></ComboBox>
-      {account && (
-        <div>
-          <>
-            {account && `Connected account: ${account}`}
-            {(!escrow && chainId) && <Button onClick={() => deployEscrow(chainId)}>Deploy Escrow</Button>}
-            {escrow && <>
-              <p>Escrow: {escrow.target as string}</p>
-              <Button onClick={() => deployNft("sample", "SAMPLE")}>Deploy Nft</Button>
-              <ComboBox value={nft} values={nfts.map(r => ({ value: r.addr.toLowerCase(), label: r.addr }))} setValue={r => setNft(r)}></ComboBox>
-              <br></br>
-              <Button onClick={() => mintToken(targetNft.addr, [targetNft.tokens.length + 1])}>Mint Token</Button>
-              <ComboBox value={token} values={targetNft?.tokens?.map(r => ({ value: r, label: r})) ?? [] } setValue={r => setToken(r.toString())}></ComboBox>
+    <div className="min-h-screen bg-gray-100 p-8">
+      <Card className="max-w-xl mx-auto ">
+        <CardHeader>
+          {account && `Connected account: ${account}`}
+          {!account && 'No account found'}
+        </CardHeader>
+        <CardContent>
+          {!account && <>
+            <h1 className="text-2xl font-bold text-gray-800">Welcome!</h1>
+            <p className="text-gray-600">Please connect to a network to start.</p>
+            <ComboBox value={chainId} values={APP_OPTION} setValue={id => connect(id)}></ComboBox>
+          </>}
+          {account && connected && (
+            <div>
+              <>
+                {(!escrow && chainId) && (
+                  <Button
+                    className="mt-4 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => deployEscrow(chainId)}
+                  >
+                    Deploy Escrow
+                  </Button>
+                )}
+                {escrow && <p>Escrow: {escrow.target as string}</p>}
+              </>
+            </div>
+          )}
+        </CardContent>
+      </Card >
 
-              { token && <Button onClick={() => sendToEscrow(targetNft.addr, token!)}>Send {token}</Button> }
+      {escrow && <>
+        <Card className="max-w-xl mx-auto my-3">
+          <CardContent>
+            <Table>
+              <TableCaption>List of Owned/Locked Tokens</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Nft</TableHead>
+                  <TableHead>TokenId</TableHead>
+                  <TableHead>Locked</TableHead>
+                  <TableHead>Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {
+                  Object.entries(nfts).flatMap(([addr, r]) => Object.entries(r.tokens).map(([rt, l]) => (
+                    <TableRow key={addr + rt}>
+                      <TableCell>{r.name}</TableCell>
+                      <TableCell>{rt}</TableCell>
+                      <TableCell>{l.exists ? 'Locked' : 'Not Locked'}</TableCell>
+                      <TableCell>Not Available</TableCell>
+                    </TableRow>
+                  )))
+                }
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
 
-            </>
-            }
-          </>
-        </div>
-      )}
-    </div>
+        <Card className="max-w-xl mx-auto my-3">
+          <CardHeader>Deploy Nft</CardHeader>
+          <CardContent className='flex flex-row'>
+            <Input type="string" placeholder='Nft Name' value={newNft} onChange={(r) => setnewNft(r.target.value)}></Input>
+            <Input type="string" placeholder='Nft Symbol' value={newNft.toUpperCase().slice(0, 3)} readOnly></Input>
+            <Button onClick={() => {
+              deployNft(newNft, newNft.toUpperCase().slice(0, 3))
+              setnewNft('')
+            }}>
+              Deploy Nft
+            </Button>
+          </CardContent>
+        </Card>
+
+        <Card className="max-w-xl mx-auto my-3">
+          <CardHeader>Mint Token</CardHeader>
+          <CardContent className="flex flex-row">
+            <ComboBox value={nft} values={Object.entries(nfts).map(([addr, r]) => ({ value: addr.toLowerCase(), label: r.name }))} setValue={r => setNft(r)}></ComboBox>
+            <Button onClick={() => mintToken(nft!, [Object.keys(nfts[nft!].tokens).length + 1])} disabled={!nft}>Mint Token</Button>
+          </CardContent>
+        </Card>
+
+        <Card className="max-w-xl mx-auto my-3">
+          <CardHeader>Lock Token</CardHeader>
+          <CardContent>
+            <ComboBox value={nft} values={Object.entries(nfts).map(([addr, r]) => ({ value: addr.toLowerCase(), label: r.name }))} setValue={r => setNft(r)}></ComboBox>
+            <ComboBox value={token} values={Object.entries(nfts[nft!]?.tokens ?? {})?.map(([r,]) => ({ value: r, label: r })) ?? []} setValue={r => setToken(r.toString())}></ComboBox>
+            {nft && token && <Button onClick={() => sendToEscrow(nft!, token!)}>Send {token}</Button>}
+          </CardContent>
+        </Card>
+
+        <Card className="max-w-xl mx-auto my-3">
+          <CardHeader>Unlock Token</CardHeader>
+          <CardContent>
+            <ComboBox value={nft} values={Object.entries(nfts).map(([addr, r]) => ({ value: addr.toLowerCase(), label: r.name }))} setValue={r => setNft(r)}></ComboBox>
+            <ComboBox value={token} values={Object.entries(nfts[nft!]?.tokens ?? {})?.map(([r,]) => ({ value: r, label: r })) ?? []} setValue={r => setToken(r.toString())}></ComboBox>
+            {nft && token && <Button onClick={() => withdrawEscrow(nft!, token!)}>Send {token}</Button>}
+          </CardContent>
+        </Card>
+      </>
+      }
+    </div >
   )
 }
--- app/src/lib/useEscrow.ts
+++ app/src/lib/useEscrow.ts
@@ -1,86 +1,139 @@
-import { AddressLike, ethers } from "ethers"
+import { AddressLike, ZeroAddress, ethers } from "ethers"
 
-import { Escrow__factory, Escrow, TestNft__factory, TestNft } from "./typechain-types"
+import { Escrow__factory, Escrow, TestNft__factory, TestNft, SafeERC20__factory } from "./typechain-types"
 
 import { useState } from "react"
 import { useSDK } from "@metamask/sdk-react"
 import { useEffect } from "react"
+import { produce } from 'immer'
 
 export function useEscrow() {
-    let { account } = useSDK()
-    let EscrowFactory = new Escrow__factory()
-    let TestNftFactory = new TestNft__factory()
+  let { account, chainId } = useSDK()
+  let EscrowFactory = new Escrow__factory()
+  let TestNftFactory = new TestNft__factory()
 
-    let [escrow, setEscrow] = useState<string | undefined>()
-    let [nfts, setNfts] = useState<{[nft: string]: string[]}>({})
-    let [userNfts, setUserNfts] = useState<[nft: string, string][]>([])
+  let [escrow, setEscrow] = useState<string | undefined>()
+  let [nfts, setNfts] = useState<{
+    [nft: string]: {
+      name: string,
+      tokens: {
+        [tokenId: string]: Escrow.TokenLoanStruct,
+      }
+    }
+  }>({})
 
-
-    useEffect(() => {
-        if (escrow) {
-            let provider = new ethers.BrowserProvider(window.ethereum!);
-            let e = Escrow__factory.connect(escrow, provider)
-            let filter = e.filters.onERC721Transfer(undefined, account)
+  useEffect(() => {
+    if (escrow && account && chainId) {
+      let provider = new ethers.BrowserProvider(window.ethereum!);
+      let jsonRpcUrl = chainId == "0x539" ? "http://127.0.0.1:8545" : "http://127.0.0.1:4444/api/infura";
+      let rpcProvider = new ethers.JsonRpcProvider(jsonRpcUrl)
+      let e = Escrow__factory.connect(escrow, rpcProvider)
+      e.addListener("onReview", async () => {
+        for (let nft in nfts) {
+          let lockedTokens = await e.lockedTokens(nft, account!)
+          for (let tokenId of lockedTokens) {
+            let tokenInfo = await e.loan(nft, account!, tokenId );
+            setNfts(
+              produce(draft => {
+                draft[nft].tokens[tokenId.toString()].loan = Number(tokenInfo.loan)
+              })
+            )
+          }
         }
-    }, [])
-    
-    const deployEscrow = async (chainId: string) => {
-        let deployed: { "Escrow#Escrow"?: string } | undefined;
-        try {
-            deployed = await import(`./deployments/chain-${BigInt(chainId).toString()}/deployed_addresses.json`).then(r => JSON.parse(r))
-        } catch (e) {
-            console.warn('no deployments found for escrow')
+      })
+      return () => { e.removeAllListeners() };
+    }
+  }, [escrow])
+
+  // useEffect(() => {
+  //     let provider = new ethers.BrowserProvider(window.ethereum!);
+  //     let jsonRpcUrl = provider._network.chainId == BigInt("0x539") ? "http://127.0.0.1:8545" : "http://127.0.0.1:4444/api/infura";
+  //     let rpcProvider = new ethers.JsonRpcProvider(jsonRpcUrl)
+
+  // }, [account])
+
+  const deployEscrow = async (chainId: string) => {
+    let deployed: { "Escrow#Escrow"?: string } | undefined;
+    try {
+      deployed = await import(`./deployments/chain-${BigInt(chainId).toString()}/deployed_addresses.json`).then(r => JSON.parse(r))
+    } catch (e) {
+      console.warn('no deployments found for escrow')
+    }
+    if (deployed?.["Escrow#Escrow"]) {
+      setEscrow(deployed["Escrow#Escrow"])
+      return;
+    }
+    let provider = new ethers.BrowserProvider(window.ethereum!);
+    let deployTx = await EscrowFactory.getDeployTransaction()
+    let signer = await provider.getSigner()
+    let res = await signer.sendTransaction(deployTx)
+    let addr = ethers.getCreateAddress(res)
+    setEscrow(addr)
+  }
+
+  const sendToEscrow = async (nftAddr: string, tokenId: string) => {
+    let provider = new ethers.BrowserProvider(window.ethereum!);
+    let signer = await provider.getSigner()
+    let nft = TestNft__factory.connect(nftAddr, signer)
+    await nft["safeTransferFrom(address,address,uint256)"](signer.address!, escrow!, tokenId)
+
+    setNfts(
+      produce(draft => {
+        draft[nftAddr].tokens[tokenId].exists = true
+      })
+    )
+  }
+
+  const withdrawEscrow = async (nftAddr: string, tokenId: string) => {
+    let provider = new ethers.BrowserProvider(window.ethereum!);
+    let signer = await provider.getSigner()
+
+    let e = Escrow__factory.connect(escrow!, signer)
+    await e.withdraw(nftAddr, tokenId)
+    let state = await e.loan(nftAddr, account!, tokenId)
+
+    setNfts(
+      produce(draft => {
+        draft[nftAddr].tokens[tokenId] = state
+      })
+    )
+  }
+
+  const deployNft = async (name: string, symbol: string) => {
+    let provider = new ethers.BrowserProvider(window.ethereum!);
+    let deployTx = await TestNftFactory.getDeployTransaction(name, symbol)
+    let signer = await provider.getSigner()
+    let res = await signer.sendTransaction(deployTx)
+    let addr = ethers.getCreateAddress(res).toLowerCase()
+
+    setNfts(produce(s => {
+      s[addr] = { tokens: {}, name }
+    }))
+  }
+
+  const mintToken = async (addr: string, tokens: number[]) => {
+    let provider = new ethers.BrowserProvider(window.ethereum!);
+    let signer = await provider.getSigner()
+    let nft = TestNft__factory.connect(addr, signer)
+
+    await nft.testMint(tokens)
+
+    setNfts(
+      produce((s) => {
+        for (let token of tokens) {
+          s[addr].tokens[BigInt(token).toString()] = { exists: false, loan: 0, token: "" }
         }
-        if (deployed?.["Escrow#Escrow"]) {
-            setEscrow(deployed["Escrow#Escrow"])
-            return;
-        }
-        let provider = new ethers.BrowserProvider(window.ethereum!);
-        let deployTx = await EscrowFactory.getDeployTransaction()
-        let signer = await provider.getSigner()
-        let res = await signer.sendTransaction(deployTx)
-        let addr = ethers.getCreateAddress(res)
-        setEscrow(addr)
-    }
+      })
+    )
+  }
 
-    const sendToEscrow = async (nftAddr: string, tokenId: string) => {
-        let provider = new ethers.BrowserProvider(window.ethereum!);
-        let signer = await provider.getSigner()
-        let nft = TestNft__factory.connect(nftAddr, signer)
-        await nft["safeTransferFrom(address,address,uint256,bytes)"](signer.address!, escrow!, tokenId, "")
-        setUserNfts(nfts => [...nfts, [nftAddr, tokenId]])
-    } 
-
-    const deployNft = async (name: string, symbol: string) => {
-        let provider = new ethers.BrowserProvider(window.ethereum!);
-        let deployTx = await TestNftFactory.getDeployTransaction(name, symbol)
-        let signer = await provider.getSigner()
-        let res = await signer.sendTransaction(deployTx)
-        let addr = ethers.getCreateAddress(res)
-        let nft = TestNft__factory.connect(addr, signer)
-        setNfts(nfts => ({...nfts, ...{[addr]: [] }}))
-    }
-
-    const mintToken = async (addr: string, tokens: number[]) => {
-        let provider = new ethers.BrowserProvider(window.ethereum!);
-        let signer = await provider.getSigner()
-        let nft = TestNft__factory.connect(addr, signer)
-
-        await nft.testMint(tokens)
-        let next = [...nfts[addr], ...tokens.map(r => r.toString(16))]
-        setNfts(nfts => ({...nfts, ...{[addr]: next }}))
-    }
-
-    return {
-        deployNft,
-        deployEscrow,
-        sendToEscrow,
-        mintToken,
-        nfts: Object.entries(nfts).map(([nft, tokens]) => ({
-            addr: nft,
-            nft: TestNft__factory.connect(nft, new ethers.BrowserProvider(window.ethereum!)),
-            tokens 
-        })),
-        escrow: escrow ? Escrow__factory.connect(escrow!) : undefined
-    }
+  return {
+    deployNft,
+    deployEscrow,
+    sendToEscrow,
+    withdrawEscrow,
+    mintToken,
+    nfts,
+    escrow: escrow ? Escrow__factory.connect(escrow!) : undefined
+  }
 }
